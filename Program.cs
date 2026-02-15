b43554e489bddbe37e2c8fcd717039fdffb25dc1
Mathis-Pain <mathis.pain76@gmail.com>
2026-02-15 16:43:53 +0100

    Condition if/else

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("quelle est ton age?");
+int age = int.Parse(Console.ReadLine() ?? "0");
+if (age<18){
+  Console.WriteLine("tu es mineur");
+}else if (age>65){
+Console.WriteLine("tu es senior");
+}else{ 
+  Console.WriteLine("tu es majeur");
+  }
